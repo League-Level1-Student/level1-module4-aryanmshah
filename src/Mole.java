10980ea39910557250e233953663051299c827ff
League Student <league@iMac-24.attlocal.net>
2019-11-27 18:26:51 -0800

    ponton

--- src/Mole.java
+++ src/Mole.java
@@ -1,6 +1,7 @@
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,19 +21,24 @@ public class Mole implements ActionListener {
 void setup() {
 	frame.setVisible(true);
 	frame.add(panel);
-	for (int i = 0; i < 25; i++) {
-		JButton button=new JButton();
-		panel.add(button);
-		button.addActionListener(this);
-	}
 	
+	drawbuttons(5);
 	
 	frame.setPreferredSize(new Dimension(250,300));
 	frame.pack();
 }
+Random randy=new Random();
 
 void drawbuttons(int a){
-	
+	int c=randy.nextInt(25);
+	for (int i = 0; i < 25; i++) {
+		JButton button=new JButton();
+		panel.add(button);
+		button.addActionListener(this);
+		if(c==i) {
+			button.setText("moole");
+		}
+	}
 	
 }
 
@@ -41,5 +47,16 @@ void drawbuttons(int a){
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
 	
+	if(!((JButton) e.getSource()).getText().equals("moole")) {
+		speak("Sorry dawg homie big man G ponton i am russsssiiiiannnn that aint moole");
+	}
+}
+
+void speak(String words) { 
+    try { 
+        Runtime.getRuntime().exec("say " + words).waitFor();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
 }
 }
